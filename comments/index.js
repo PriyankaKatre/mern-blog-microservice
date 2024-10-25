82d331a2e397b6d8c4791153b04fb22c90b5378f
Priyanka Katre <priyankack1707@gmail.com>
2024-10-25 22:54:26 +0100

    add post and comment backend route created

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "dotenv/config";
+import { randomBytes } from "crypto";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+let commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id]);
+});
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { comment } = req.body;
+
+  let comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, comment });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+const port = process.env.PORT || 4001;
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,28 @@
 import express from 'express'
 import 'dotenv/config'
+import { randomBytes } from 'crypto';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
+
+let posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts)
 })
 app.post('/posts', (req, res) => {
-
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body;
+    console.log(req.body);
+    posts[id]= {
+        id,
+        title: title
+    }
+    console.log("posts", posts);
+    res.status(201).send(posts[id])
 })
+
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
